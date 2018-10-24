e74d37e931c7dd2f94f37b772f5798f71563ac60
Vince Andrew Pimentel <contact@vincep.me>
2018-10-24 22:41:45 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,13 @@
 class Backer
   attr_accessor :name, :backed_projects
-  
+
   def initialize(name)
     @name = name
     @backed_projects = Array.new
+  end
+
+  def back_projects(Project)
+    @backed_projects << Project
   end
 
 end
